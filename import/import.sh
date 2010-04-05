43a4649f56328eb4a3fc5437182ce929b892aba9
Miroff <mr.miroff@gmail.com>
2010-04-05 14:53:42 +0700

    fixed CR

--- import/import.sh
+++ import/import.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget -c -t0 http://gis-lab.info/data/osm/novosib/novosib.osm.bz2time osm2pgsql -l -c -d osm -G -S default.style novosib.osm.bz2
+wget -c -t0 http://gis-lab.info/data/osm/novosib/novosib.osm.bz2
+osm2pgsql -l -c -d osm -G -S default.style novosib.osm.bz2
