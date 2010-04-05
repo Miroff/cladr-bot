db8a62f85896b5069cc2bbdaa2f9594ced40f47e
jean <jean@jplinux.localdomain>
2010-04-05 22:01:14 +0400

    обернул каждый город с свой try..except на пробу

--- process-streets.py
+++ process-streets.py
@@ -61,6 +61,17 @@ def prepare_name(name):
   
   return name.strip()
 
+def compact(key, name, cladr_code, cladr_name, cladr_suffix, postcode, osm_id = 0): 
+  return {
+        'key': key, 
+        'name': name, 
+        'cladr:code': cladr_code, 
+        'cladr:name': cladr_name,
+        'cladr:suffix': cladr_suffix,
+        'addr:postcode': postcode,
+        'osm_id': osm_id,
+       }
+
 def process(api_user, api_password, city_polygon_id, city_cladr_code, db_host, db_port, db_name, db_user, db_password, dry_run, quiet):
   #Fetch data
   if not quiet: print "Fetching data"
@@ -68,8 +79,8 @@ def process(api_user, api_password, city_polygon_id, city_cladr_code, db_host, d
   cladrDB = CladrDB(db_host, db_port, db_name, db_user, db_password, quiet)
   osmDB = OSMDB(db_host, db_port, db_name, db_user, db_password, quiet)
 
-  (cladr_by_name, cladr_by_code) = cladrDB.load_data( prepare_name, city_cladr_code)
-  (osm_data, osm_by_code) = osmDB.load_data( prepare_name, city_polygon_id)
+  (cladr_by_name, cladr_by_code) = cladrDB.load_data( prepare_name, compact, city_cladr_code)
+  (osm_data, osm_by_code) = osmDB.load_data( prepare_name, compact, city_polygon_id)
 
   cladrDB.close()
   osmDB.close()
@@ -105,7 +116,11 @@ if options.city_osm_id == None or options.city_cladr_code == None:
   for (osm_id, cladr) in osmDB.query_all_cities():
     if not options.quiet:
       print "Processing city #%s (%s)" % (cladr, osm_id)
-    process(options.api_user.decode('utf-8'), options.api_password.decode('utf-8'), str(osm_id), cladr, options.db_host, options.db_port, options.db_name, options.db_user, options.db_password, options.dry_run, options.quiet)
+    
+    try:
+	  process(options.api_user.decode('utf-8'), options.api_password.decode('utf-8'), str(osm_id), cladr, options.db_host, options.db_port, options.db_name, options.db_user, options.db_password, options.dry_run, options.quiet)
+    except:
+	  print "Died in city #%s (%s)" % (cladr, osm_id)	
 
 else:
   process(options.api_user.decode('utf-8'), options.api_password.decode('utf-8'), options.city_osm_id, options.city_cladr_code, options.db_host, options.db_port, options.db_name, options.db_user, options.db_password, options.dry_run, options.quiet)
