1f7c64e14871461b71aa1464324b59acdf57b9d2
Miroff <mr.miroff@gmail.com>
2010-04-11 10:48:29 +0700

    Fixed import

--- import/import.sh
+++ import/import.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 wget -c -t0 http://gis-lab.info/data/osm/novosib/novosib.osm.bz2
-osm2pgsql -l -c -d osm -G -S default.style novosib.osm.bz2
+osm2pgsql -l -c -d osm -G -S default.style --prefix=osm -x novosib.osm.bz2
+psql osm <post-import.sql
+rm novosib.osm.bz2
